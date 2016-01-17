0f7d4863a4f8bc753f406b24c9a454bc46cca609
Sergey Donin <sergey.donin@gmail.com>
2016-01-17 15:49:12 +0300

    Zip module contens

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,34 @@ config = require('./gulp-config.json')
 // File systems
 fs          = require('fs'),
 path        = require('path'),
+gulpPath    = require('gulp-path'),
 merge       = require('merge-stream'),
+deb         = require('gulp-debug'),
 parseString = require('xml2js').parseString;
 ver         = {};
+
+var assets = gulpPath.Base({
+        src: './app',
+        dest: config.releaseDir
+    });
+
+// Setting paths
+var paths = {
+        // img.dest = './resources/images/'
+        // img.src = './assets/images/**/*'
+//        img: new assets.Path('images'),
+        // js.dest = './resources/js/'
+        // js.src = './assets/js/**/*.js'
+//        js: new assets.Path('js', 'js'),
+        // fonts.dest = './public/fonts/'
+        // fonts.src = './public/fonts/**/*'
+//        fonts: new public.Path('fonts'),
+        // controllers.dest = './controllers/'
+        // controllers.src = './controllers/**/*.js'
+        app: new assets.Path( config.packageFiles )
+    };
+
+//console.log('assets:' + paths.app.files() );
 
 parseString(fs.readFileSync('app/mod_doyandexmetrika.xml', 'ascii'), function(err, result) {
 
@@ -25,7 +50,13 @@ parseString(fs.readFileSync('app/mod_doyandexmetrika.xml', 'ascii'), function(er
 
 gulp.task('hello', function() {
 
-  console.log( ver );
-  console.log( config.name );
+// Start up log
+  gutil.log(gutil.colors.white.bgGreen('Preparing release for version ' + ver));
+
+  gulp.src( config.packageFiles, { base: 'app' } )
+    .pipe(zip('mod_' + config.name + '-v' + ver + '_testgulp' + '.zip'))
+    .pipe(gulp.dest(config.releaseDir));
+
+  gutil.log(gutil.colors.white.bgGreen('Component packages are ready at ' + config.releaseDir));
 
 });
